559f22ecc16eaee5841ba314ccf6f3ec47e739ca
zihads <zhs@bu.edu>
2019-02-28 12:18:43 -0500

    publish page

---
+++ precache-manifest.38eefbe5922bf34e0baae49e39f85db4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bc8cc4eae3d3822562e825fb5cec5536",
+    "url": "/static/media/blue-marker.bc8cc4ea.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "24315ad4c649e23607f5",
+    "url": "/static/js/main.24315ad4.chunk.js"
+  },
+  {
+    "revision": "d4b9d37e242168614611",
+    "url": "/static/js/1.d4b9d37e.chunk.js"
+  },
+  {
+    "revision": "24315ad4c649e23607f5",
+    "url": "/static/css/main.932e4aa7.chunk.css"
+  },
+  {
+    "revision": "d4b9d37e242168614611",
+    "url": "/static/css/1.29b2c4f4.chunk.css"
+  },
+  {
+    "revision": "5fab29ee8eeb8a084ccbfdc3edfc0a67",
+    "url": "/index.html"
+  }
+];
